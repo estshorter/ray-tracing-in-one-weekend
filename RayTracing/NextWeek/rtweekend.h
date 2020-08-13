3d9738a233db245b40fd1c19acd29fe736d09824
estshorter <1430311+estshorter@users.noreply.github.com>
2020-08-13 19:25:51 +0900

    finish netxtweek

---
+++ RayTracing/NextWeek/rtweekend.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <limits>
+#include <memory>
+#include <random>
+
+// using
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// 定数
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.1415926535897932385;
+
+// ユーティリティ関数
+inline double degrees_to_radians(double degrees) {
+	return degrees * pi / 180;
+}
+
+inline double random_double() {
+	static std::uniform_real_distribution<double> dist(0.0, 1.0);
+	static std::random_device seed_gen;
+	static std::mt19937 engine(seed_gen());
+	return dist(engine);
+}
+
+inline double random_double(double min, double max) {
+	// Returns a random real in [min,max).
+	return min + (max - min) * random_double();
+}
+
+inline int random_int(int min, int max) {
+	// Returns a random integer in [min,max].
+	return static_cast<int>(random_double(min, max + 1));
+}
+
+// 共通ヘッダー
+#include "ray.h"
+#include "vec3.h"
---
+++ RayTracing/NextWeek/sphere.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "hittable.h"
+#include "vec3.h"
+#include "aabb.h"
+
+void get_sphere_uv(const vec3& p, double& u, double& v) {
+	auto phi = atan2(p.z(), p.x());
+	auto theta = asin(p.y());
+	u = 1 - (phi + pi) / (2 * pi);
+	v = (theta + pi / 2) / pi;
+}
+
+class sphere : public hittable {
+public:
+	sphere() {}
+	sphere(point3 cen, double r, shared_ptr<material> m)
+		: center(cen), radius(r), mat_ptr(m) {}
+
+	virtual bool hit(
+		const ray& r, double tmin, double tmax, hit_record& rec
+	) const;
+	virtual bool bounding_box(double t0, double t1, aabb& output_box) const override;
+
+public:
+	point3 center;
+	double radius;
+	shared_ptr<material> mat_ptr;
+};
+
+bool sphere::hit(
+	const ray& r, double t_min, double t_max, hit_record& rec
+) const {
+	vec3 oc = r.origin() - center;
+	auto a = r.direction().length_squared();
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant = half_b * half_b - a * c;
+
+	if (discriminant > 0) {
+		auto root = sqrt(discriminant);
+		auto temp = (-half_b - root) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.at(rec.t);
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
+			get_sphere_uv((rec.p - center) / radius, rec.u, rec.v);
+			rec.mat_ptr = mat_ptr;
+			return true;
+		}
+		temp = (-half_b + root) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.at(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
+			get_sphere_uv((rec.p - center) / radius, rec.u, rec.v);
+			rec.mat_ptr = mat_ptr;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool sphere::bounding_box(double t0, double t1, aabb& output_box) const {
+	output_box = aabb(
+		center - vec3(radius, radius, radius),
+		center + vec3(radius, radius, radius));
+	return true;
+}
